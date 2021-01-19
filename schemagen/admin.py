631ffa02805c1d7054fc3a0e48e0510a2a8aa427
Vera Vovk <vovkverait@gmail.com>
2021-01-19 20:55:15 +0200

    chaged Admin: TabularInline

--- schemagen/admin.py
+++ schemagen/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import User, Field, Schema
 
 admin.site.register(User)
-admin.site.register(Field)
-admin.site.register(Schema)
+#admin.site.register(Field)
+#admin.site.register(Schema)
+
+class FieldInline(admin.TabularInline):
+    model = Field
+
+class SchemaAdmin(admin.ModelAdmin):
+    fields = (
+        'user',
+        'name',
+    )
+    inlines = [
+        FieldInline,
+    ]
+
+admin.site.register(Schema, SchemaAdmin)
